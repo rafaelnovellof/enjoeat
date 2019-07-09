00cf272b6f18e5d946d93e7e4ef50fade914feb6
Rafael Novello <rafaelnovello@live.com>
2019-07-09 11:16:12 -0300

    First commit

---
+++ features/restaurantes.feature
@@ -0,0 +1,32 @@
+#language: pt
+
+Funcionalidade: Restaurantes    
+    Para que eu possa saber quais os restaurantes disponíveis com o tempo de entrega e nota de avaliação
+    Sendo um usuário que deseja pedir comida
+    Posso acessar a lista de restaurantes
+
+    Contexto: Lista de restaurantes
+    
+        Dado que eu tenho uma lista de restaurantes
+
+       Cenario: Todos os restaurantes
+
+        Quando acesso a lista de restaurantes
+        Então vejo todas as opções disponíveis
+    
+    Cenario: Categorias
+
+        Quando acesso a lista de restaurantes
+        Então cada restaurante deve exibir sua categoria
+
+    @Temp #cucumber -t @temp
+
+    Cenario: Tempo de entrega
+
+        Quando acesso a lista de restaurantes
+        Então cada restaurante deve exibir o tempo de entrega
+
+    Cenario: Nota de avaliação
+
+        Quando acesso a lista de restaurantes
+        Então cada restaurante deve exibir sua nota de avaliação
---
+++ features/step_definitions/restaurantes_steps.rb
@@ -0,0 +1,42 @@
+  Dado("que eu tenho uma lista de restaurantes") do
+    @restaurant_data = [
+        { name: 'Bread & Bakery', category: 'Padaria', delivery_time: '25 minutos', rating: 4.9 }, 
+        { name: 'Burger House', category: 'Hamburgers', delivery_time: '30 minutos', rating: 3.5 },
+        { name: 'Cofee Corner', category: 'Cafeteria', delivery_time: '20 minutos', rating: 4.8 }
+    ]
+  end
+
+  Quando("acesso a lista de restaurantes") do
+      visit '/restaurants'
+  end
+  
+  Então("vejo todas as opções disponíveis") do
+   restaurantes = all('.restaurant-item') # o método devolve uma coleção de elementos (array)
+   expect(restaurantes.size).to be > 1
+  end
+  
+  Então("cada restaurante deve exibir sua categoria") do
+    restaurants = all('.restaurant-item')
+
+    @restaurant_data.each_with_index do |value, index|
+         expect(restaurants[index]).to have_text value[:category]
+    end 
+
+  end
+  
+  Então("cada restaurante deve exibir o tempo de entrega") do
+    restaurants = all('.restaurant-item')
+
+    @restaurant_data.each_with_index do |value, index| 
+        expect(restaurants[index]).to have_text value[:delivery_time]
+    end 
+
+  end
+  
+  Então("cada restaurante deve exibir sua nota de avaliação") do
+    restaurants = all('.restaurant-item')
+
+    @restaurant_data.each_with_index do |value, index| 
+        expect(restaurants[index]).to have_text value[:rating]
+    end 
+  end
