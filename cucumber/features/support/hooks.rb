6e0dc08f2a518747984e19e3480fead09b21c376
Rafael Novello <rafaelnovello@live.com>
2019-07-10 17:19:11 -0300

    funfou o report!

--- cucumber/features/support/hooks.rb
+++ cucumber/features/support/hooks.rb
@@ -1,21 +1,12 @@
+#utf-8
+
 #Comment: Hook to resize the window before each step
 
 # Before do 
 #     page.current_window_resize_to(1440, 900)
 # end
 
-After do
-    File.open("screenshot.png", "w") { |file| file << "foo" }
-    embed("screenshot.png", "image/png")
-    data = "YWJj"
-    embed(data, "mime-type;base64")
-end
-
-
-
-    
-
-
-#   temp_shot = page.save_screenshot("log/temp_shot.png")
-#     screenshot = Base64.encode64(File.open(temp_shot).read)
-#     embed(screenshot, "image/png", "SCREENSHOT")
+After do |scenario|
+    shot = page.save_screenshot("log/#{scenario.__id__}.png")
+    embed(shot, "image/png", "SCREENSHOT")
+  end
