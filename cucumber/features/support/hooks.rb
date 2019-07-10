dd19c921aeb1199605cb0a36d161b5ce1f90cdb6
Rafael Novello <rafaelnovello@live.com>
2019-07-10 15:10:05 -0300

    change hooks screenshot

--- cucumber/features/support/hooks.rb
+++ cucumber/features/support/hooks.rb
@@ -4,8 +4,18 @@
 #     page.current_window_resize_to(1440, 900)
 # end
 
-After do 
-    temp_shot = page.save_screenshot("log/temp_shot.png")
-    screenshot = Base64.encode64(File.open(temp_shot).read)
-    embed(screenshot, "image/png", "Screenshot")
+After do
+    File.open("screenshot.png", "w") { |file| file << "foo" }
+    embed("screenshot.png", "image/png")
+    data = "YWJj"
+    embed(data, "mime-type;base64")
 end
+
+
+
+    
+
+
+#   temp_shot = page.save_screenshot("log/temp_shot.png")
+#     screenshot = Base64.encode64(File.open(temp_shot).read)
+#     embed(screenshot, "image/png", "SCREENSHOT")
