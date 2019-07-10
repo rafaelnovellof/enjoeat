4b2123cb9cce959c6ac9eab3268df67f2c7388c1
Rafael Novello <rafaelnovello@live.com>
2019-07-10 11:15:56 -0300

    env changes

--- cucumber/features/support/env.rb
+++ cucumber/features/support/env.rb
@@ -2,65 +2,26 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
+##---------------------------------------------------------------------------------##
 
-Capybara.register_driver :selenium do |app|
-
-    args = %w[headless --no-sandbox disable-gpu window-size=1440,900 --disable-dev-shm-usage]
-
-    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-        'chromeOptions' => {
-            'args' => args
-        }
-    )
-
-Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: caps)
-
-end
-
-# Capybara.register_driver :poltergeist do |app|
-
-#     options = {
-#         timeout: 3.minutes,
-#         phantomjs_options: [
-#        'proxytype=none',
-#        'loadimages=no',
-#        'ignoresslerrors=yes',
-#        'sslprotocol=any',
-#        'websecurity=false'
-#         ]
-#      }
-#      Capybara::Poltergeist::Driver.new(app, options)
-# end
-
-# Capybara.javascript_driver = :poltergeist
-
-#  Capybara.register_driver :chrome do |app|
-
-#    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#      chromeOptions: {
-#        args: %w[ nosandbox headless disablepopupblocking disablegpu windowsize=1440,900]
-#      }
-#    )
- 
-#    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
-
-#  end
-
-# Capybara::Screenshot.register_driver :chrome do |driver, path|
-    
-#     driver.save_screenshot(path)
-    
-# end
-    
-#     Capybara.javascript_driver = :chrome
-    
-#     Capybara.default_max_wait_time = 10
+    #DEVEMOS USAR ESSA CONFIGURAÇÃO PARA O GOOGLE CHROME NÃO QUBRAR (CRASH)
+    Capybara.register_driver :selenium_chrome_headless do |app|
+        chrome_options =  Selenium::WebDriver::Chrome::Options.new.tap do |options|
+            options.add_argument "--headless"
+            options.add_argument "--disable-gpu"
+            options.add_argument "--no-sandbox"
+            options.add_argument "--disable-site-isolation-trials"
+        end
+        Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+    end
+        
+##---------------------------------------------------------------------------------##
 
 Capybara.configure do |config|
 
-    config.default_driver = :selenium
-    config.app_host = 'http://enjoeatsp1.herokuapp.com'
+    config.default_driver = :selenium_chrome_headless
+    config.app_host = 'http://enjoeat-sp1.herokuapp.com'
+    Capybara.default_max_wait_time = 30
 
 end
 
-Capybara.default_max_wait_time = 10
