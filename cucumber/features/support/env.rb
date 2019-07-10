8df700b1f0600a0842604755660797fc7a0846c8
Rafael Novello <rafaelnovello@live.com>
2019-07-09 22:46:01 -0300

    chrome headless

--- cucumber/features/support/env.rb
+++ cucumber/features/support/env.rb
@@ -2,19 +2,24 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
-# Capybara.register_driver :selenium do |app|
-#     args = %w[window-size=1440,900 headless disable-gpu]
 
-#     caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-#         'chromeOptions' => {
-#             'args' => args
-#         }
+Capybara.register_driver :selenium do |app|
 
-# Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: caps)
+    args = %w[window-size=1440,900 headless disable-gpu]
 
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+        'chromeOptions' => {
+            'args' => args
+        }
+    )
+    
+Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: caps)
+
+end
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+
+    config.default_driver = :selenium
     config.app_host = 'http://enjoeat-sp1.herokuapp.com'
 
 end
